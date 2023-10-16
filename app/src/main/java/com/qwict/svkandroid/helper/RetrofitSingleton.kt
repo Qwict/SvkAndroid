463def5b38e5cc8dff0a263dd1665a43453e1e03
jorisvanduyse <jorisduyse@gmail.com>
2023-10-16 16:29:52 +0200

    ip for api must not be changed anymore

--- app/src/main/java/com/qwict/svkandroid/helper/RetrofitSingleton.kt
+++ app/src/main/java/com/qwict/svkandroid/helper/RetrofitSingleton.kt
@@ -5,7 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitSingleton {
     private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("http://IP_OFF_YOUR_LAPOT/v1/")
+//        .baseUrl("http://IP_OFF_YOUR_LAPOT/v1/") // if you do not use virtual devcie this must be your laptop's IP address
+        .baseUrl("http://10.0.2.2:9010/v1/")
         .addConverterFactory(GsonConverterFactory.create())
 //        TODO: We need to add an interceptor... (to add token to header of each request)
 //        So that we don't have to propdirll it form the viewModel each time...
