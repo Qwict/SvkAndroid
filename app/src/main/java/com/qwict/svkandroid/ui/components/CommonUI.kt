5a71884ee7df172ac7e8607b65ab1004a4e456d5
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-10-16 14:32:25 +0200

    Adding logo to landing page (AuthenticationScreen)

--- app/src/main/java/com/qwict/svkandroid/ui/components/CommonUI.kt
+++ app/src/main/java/com/qwict/svkandroid/ui/components/CommonUI.kt
@@ -1,11 +1,22 @@
 package com.qwict.svkandroid.ui.components
 
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun Title(
@@ -18,5 +29,18 @@ fun Title(
             fontWeight = FontWeight.Bold,
             fontSize = 30.sp,
         ),
+    )
+}
+
+@Composable
+fun IndeterminateCircularIndicator() {
+    var loading by remember { mutableStateOf(true) }
+
+    if (!loading) return
+
+    CircularProgressIndicator(
+        modifier = Modifier.width(64.dp),
+        color = MaterialTheme.colorScheme.primary,
+//        trackColor = MaterialTheme.colorScheme.secondary,
     )
 }
--- app/src/main/java/com/qwict/svkandroid/ui/screens/AuthenticationScreen.kt
+++ app/src/main/java/com/qwict/svkandroid/ui/screens/AuthenticationScreen.kt
@@ -1,24 +1,63 @@
 package com.qwict.svkandroid.ui.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.qwict.svkandroid.R
 import com.qwict.svkandroid.ui.MainViewModel
+import com.qwict.svkandroid.ui.components.IndeterminateCircularIndicator
+import com.qwict.svkandroid.ui.theme.SvkAndroidTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 // TODO: this is an implementation for auth0, we will probably need to change this to our own implementation
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun AuthenticationScreen(
     viewModel: MainViewModel,
     userAuthNav: () -> Unit,
     loginNav: () -> Unit,
 ) {
+
     if (viewModel.userIsAuthenticated) {
         userAuthNav()
     } else {
         loginNav()
     }
-    Column {
-        Text(text = "Authentication Screen", color = MaterialTheme.colorScheme.onSurface)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.svk_logo_zonder_slogan),
+            contentDescription = "Loading"
+        )
+//        Spacer(modifier = Modifier.height(32.dp))
+//        IndeterminateCircularIndicator()
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun AuthenticationScreenPreview() {
+    SvkAndroidTheme(darkTheme = false) {
+        AuthenticationScreen(
+            viewModel = MainViewModel(),
+            userAuthNav = {},
+            loginNav = {},
+        )
     }
 }
