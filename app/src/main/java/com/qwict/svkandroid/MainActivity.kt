c799101568b91d4e5c2991f9c96f381e03533bae
jorisvanduyse <jorisduyse@gmail.com>
2023-10-22 10:55:32 +0200

    v0.3.2 -> patched bug with decodedToken, tokenCheck onCreate

--- app/src/main/java/com/qwict/svkandroid/MainActivity.kt
+++ app/src/main/java/com/qwict/svkandroid/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainViewModel.setContext(this)
+        getTokenFromSharedPrefs(mainViewModel, applicationContext)
         setContent {
             // A surface container using the 'background' color from the theme
             SvkAndroidTheme {
