7908cf5ac4fd2b71d9e8df098bf75fd289d8898a
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-10-23 12:04:13 +0200

    V0.4.1 -> edit screen patch

--- app/src/main/java/com/qwict/svkandroid/ui/screens/EditScreen.kt
+++ app/src/main/java/com/qwict/svkandroid/ui/screens/EditScreen.kt
@@ -1,9 +1,9 @@
 package com.qwict.svkandroid.ui.screens
 
 import android.util.Log
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -21,13 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.qwict.svkandroid.R
 import com.qwict.svkandroid.ui.theme.SVKTextfield
-import kotlin.random.Random
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,7 +42,7 @@ fun EditScreen(barcodeValue: String?, nextNav: () -> Unit) {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = "RouteNummer 654481519849")
+                    Text(text = "Laadbon 654481519849")
                 },
                 colors = TopAppBarDefaults.smallTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -58,10 +55,11 @@ fun EditScreen(barcodeValue: String?, nextNav: () -> Unit) {
 
         Column(
             modifier = Modifier
-                .fillMaxSize() // Make the Column fill the available space
+                .fillMaxWidth()
+                .fillMaxHeight(0.75f)
                 .padding(values)
                 .padding(top = 60.dp),
-
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Textfields()
@@ -83,22 +81,22 @@ fun Textfields() {
     var nummerplaat by remember { mutableStateOf("") }
     var chauffeur by remember { mutableStateOf("") }
 
-    Image(
-        painter = rememberAsyncImagePainter(
-            model = "https://picsum.photos/seed/${Random.nextInt()}/900/500",
-        ),
-        contentDescription = null,
-        modifier = Modifier
-            .clip(MaterialTheme.shapes.large)
-            .fillMaxWidth(1f)
-            .padding(bottom = 20.dp),
-
-    )
+//    Image(
+//        painter = rememberAsyncImagePainter(
+//            model = "https://picsum.photos/seed/${Random.nextInt()}/900/500",
+//        ),
+//        contentDescription = null,
+//        modifier = Modifier
+//            .clip(MaterialTheme.shapes.large)
+//            .fillMaxWidth(1f)
+//            .padding(bottom = 20.dp),
+//
+//    )
     SVKTextfield {
         OutlinedTextField(
             value = laadbonNummer,
             onValueChange = { laadbonNummer = it },
-            label = { Text("RouteNummer") },
+            label = { Text("Laadnummer") },
             modifier = Modifier.padding(bottom = 5.dp),
         )
     }
