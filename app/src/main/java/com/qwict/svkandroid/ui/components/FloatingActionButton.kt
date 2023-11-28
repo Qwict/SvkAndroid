4c9c8d99ce458d968392b2174a8ec861964bcddc
Alexander Schatteman <alexander.schatteman@student.hogent.be>
2023-11-28 15:44:33 +0100

    V0.9.5 fix fab

--- app/src/main/java/com/qwict/svkandroid/ui/components/FloatingActionButton.kt
+++ app/src/main/java/com/qwict/svkandroid/ui/components/FloatingActionButton.kt
@@ -11,21 +11,27 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -62,11 +68,12 @@ fun MultiFloatingButton(
     ) {
         if (transition.currentState == MultiFloatingState.Expanded) {
             Card(
+                shape = RoundedCornerShape(16.dp),
                 colors = CardDefaults.cardColors(
                     containerColor = MaterialTheme.colorScheme.onTertiary,
                 ),
                 modifier = Modifier
-                    .padding(bottom = 20.dp)
+                    .padding(bottom = 16.dp)
                     .alpha(
                         animateFloatAsState(
                             targetValue = alpha,
@@ -74,7 +81,7 @@ fun MultiFloatingButton(
                             label = "",
                         ).value,
                     )
-                    .width(170.dp),
+                    .width(190.dp),
                 elevation = CardDefaults.cardElevation(8.dp),
             ) {
                 items.forEach {
@@ -83,36 +90,45 @@ fun MultiFloatingButton(
                         onMinFabItemClick = { minFabItem ->
                             when (minFabItem.identifier) {
                                 Identifier.CameraFab.name -> {
-                                    // TODO: Start Camera
                                     navigateToPhotoRoute()
                                 }
 
                                 Identifier.AddLoadFab.name -> {
                                     openAddCargoNumberDialog()
-//                                    navigateToScanRoute()
                                 }
                             }
                         },
                         alpha = alpha,
                     )
-                    Spacer(modifier = Modifier.size(12.dp))
+                    if (it != items.last()) Divider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(1.dp),
+                        color = MaterialTheme.colorScheme.primary
+                    )
                 }
             }
         }
 
-        FloatingActionButton(onClick = {
-            onMultiFabStateChange(
-                if (transition.currentState == MultiFloatingState.Expanded) {
-                    MultiFloatingState.Collapsed
-                } else {
-                    MultiFloatingState.Expanded
-                },
-            )
-        }) {
+        FloatingActionButton(modifier = Modifier
+            .height(70.dp)
+            .width(70.dp),
+
+            onClick = {
+                onMultiFabStateChange(
+                    if (transition.currentState == MultiFloatingState.Expanded) {
+                        MultiFloatingState.Collapsed
+                    } else {
+                        MultiFloatingState.Expanded
+                    },
+                )
+            }) {
             Icon(
                 imageVector = Icons.Default.Add,
                 contentDescription = null,
-                modifier = Modifier.rotate(rotate),
+                modifier = Modifier
+                    .rotate(rotate)
+                    .fillMaxSize(0.6f),
             )
         }
     }
@@ -125,11 +141,21 @@ fun MinFab(
     alpha: Float,
     showLabel: Boolean = true,
     onMinFabItemClick: (MinFabItem) -> Unit,
-
 ) {
     Row(
-        Modifier
-            .padding(8.dp)
+        modifier = Modifier
+
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = {
+                    onMinFabItemClick.invoke(item)
+                },
+                indication = rememberRipple(
+                    bounded = true,
+                    radius = 252.dp,
+                    color = MaterialTheme.colorScheme.secondary,
+                ),
+            )
             .alpha(
                 animateFloatAsState(
                     targetValue = alpha,
@@ -138,52 +164,43 @@ fun MinFab(
                 ).value,
             ),
     ) {
-        if (showLabel) {
-            Text(
-                text = item.label,
-                color = MaterialTheme.colorScheme.primary,
+        Row(modifier = Modifier.padding(16.dp)) {
 
-                fontSize = 24.sp,
 
-                fontWeight = FontWeight.Bold,
+            if (showLabel) {
+                Text(
+                    text = item.label,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+
+                        .alpha(
+                            animateFloatAsState(
+                                targetValue = alpha,
+                                animationSpec = tween(100),
+                                label = "",
+                            ).value,
+                        )
+                        .fillMaxWidth(0.8f)
+                )
+            }
+
+            Canvas(
                 modifier = Modifier
-                    .alpha(
-                        animateFloatAsState(
-                            targetValue = alpha,
-                            animationSpec = tween(100),
-                            label = "",
-                        ).value,
-                    )
-                    .fillMaxWidth(0.8f),
+                    .size(32.dp)
 
-            )
-            Spacer(modifier = Modifier.size(4.dp))
-        }
-
-        Canvas(
-            modifier = Modifier
-                .size(16.dp)
-                .clickable(
-                    interactionSource = MutableInteractionSource(),
-                    onClick = {
-                        onMinFabItemClick.invoke(item)
-                    },
-                    indication = rememberRipple(
-                        bounded = false,
-                        radius = 20.dp,
-                        color = MaterialTheme.colorScheme.onSurface,
-
+            ) {
+                drawImage(
+                    image = item.icon,
+                    topLeft = Offset(
+                        center.x - (item.icon.width / 2),
+                        center.y - (item.icon.width / 2),
                     ),
-                ),
-        ) {
-            drawImage(
-                image = item.icon,
-                topLeft = Offset(
-                    center.x - (item.icon.width / 2),
-                    center.y - (item.icon.width / 2),
-                ),
-                alpha = alpha,
-            )
+                    alpha = alpha,
+                )
+            }
         }
     }
 }
+
